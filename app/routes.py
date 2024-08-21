ed14773076a4f8c1ccd731e203d973706ea31edf
emiliosolanoo21 <85262580+emiliosolanoo21@users.noreply.github.com>
2024-08-21 02:08:40 -0600

    Registration successful

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,48 @@
 from flask import Flask, render_template, request, redirect, url_for
-from app import app
+import slixmpp
+import asyncio
+import multiprocessing
 from app.xmpp_client import XMPPClient
+from app import app
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+class RegisterXMPPClient(slixmpp.ClientXMPP):
+    def __init__(self, username, password):
+        super().__init__(username + '@alumchat.lol', password)
+        self.add_event_handler("session_start", self.start)
+
+    async def start(self, event):
+        self.send_presence()
+        await self.get_roster()
+        self.disconnect()
+
+    async def async_connect_and_run(self):
+        self.connect()
+        await self.process(forever=False)
+
+def run_xmpp_client(username, password):
+    xmpp_client = RegisterXMPPClient(username, password)
+    asyncio.run(xmpp_client.async_connect_and_run())
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Run the XMPP client in a separate process
+        process = multiprocessing.Process(target=run_xmpp_client, args=(username, password))
+        process.start()
+        process.join()
+
+        message = "Registration successful"
+
+        return render_template('register.html', message=message)
+
+    return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
